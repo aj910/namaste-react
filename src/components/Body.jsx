0a158ec7f38c40cd591d7e358e25e3d8c4a5a1e4
aj910 <anmolj2009@gmail.com>
2024-02-07 12:06:15 +0530

    react folder structure

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,16 +2,12 @@ import React from "react";
 import RestaurantCard from "./RestaurantCard";
 import resFoodList from "../utils/mockData";
 
-const displayData = (a, b) => {
-    alert(`Sum is ${a + b}`);
-}
-
 
 const Body = () => {
     return (
         <div className="container">
             <div className="filter-cont">
-               <button onClick={displayData} className="filter-btn">Top Rated Restaurants</button>
+               <button onClick={() => {}} className="filter-btn">Top Rated Restaurants</button>
             </div>
             <div className="foodCard-container">
              {
